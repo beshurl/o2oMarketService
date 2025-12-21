91f44c8c0f6fe0363bac8ed41db09c947d5c11b7
jin_____w <technical1500@gmail.com>
2025-12-22 03:37:48 +0900

    getProductsByCategory - categoryId가 0이면 전체 상품 목록 조회하도록 수정

--- src/main/java/com/ssafy/o2omystore/service/CategoryServiceImpl.java
+++ src/main/java/com/ssafy/o2omystore/service/CategoryServiceImpl.java
@@ -5,19 +5,27 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.o2omystore.dao.CategoryDao;
+import com.ssafy.o2omystore.dao.ProductDao;
 import com.ssafy.o2omystore.dto.Product;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
 	
 	private final CategoryDao categoryDao;
+	private final ProductService productService;
 	
-	public CategoryServiceImpl(CategoryDao categoryDao) {
+	public CategoryServiceImpl(CategoryDao categoryDao, ProductService productService) {
 		this.categoryDao = categoryDao;
+		this.productService = productService;
 	}
 
 	@Override
 	public List<Product> getProductsByCategory(int categoryId) {
+		
+		if (categoryId == 0) {
+			return productService.getAllProducts();
+		}
+		
 		return categoryDao.selectProductsByCategory(categoryId);
 	}
 
